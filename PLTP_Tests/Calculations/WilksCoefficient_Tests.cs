2e1c6124da02a1d2922a8488c7fd37f392afe3b2
Jack Perry <jack.perry.2015@my.ntu.ac.uk>
2020-09-30 23:45:12 +0100

    Wilks Coefficient Tests

---
+++ PLTP_Tests/Calculations/WilksCoefficient_Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using PLPT;
+
+namespace PLTP_Tests.Calculations
+{
+    [TestFixture]
+    public class WilksCoefficient_Tests
+    {
+        // SUT
+        private WilksCoefficient _wilksCoefficient;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _wilksCoefficient = new WilksCoefficient();
+        }
+
+        [TestCase(700, 100, 427)]
+        [TestCase(150, 70, 113)]
+        [TestCase(1200, 99, 734)]
+        public void CalculateCoefficient_PersonIsMale_ReturnsWilksForMale(int total, int bodyweight, int expectedResult)
+        {
+            int result = _wilksCoefficient.CalculateCoefficient(total, bodyweight, true);
+            Assert.That(result == expectedResult);
+        }
+    }
+}
